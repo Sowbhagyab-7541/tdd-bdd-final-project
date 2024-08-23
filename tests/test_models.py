123cc969d74320c5324dab4bf7a9c4cfad0f74db
Sowbhagyab-7541 <sowbhagya.byrappa@gmail.com>
2024-08-23 22:13:13 +0530

    Update test_models.py
    Test Case to Find a Product by Availability

--- tests/test_models.py
+++ tests/test_models.py
@@ -167,3 +167,15 @@ def test_delete_a_product(self):
         self.assertEqual(found.count(), count)
         for product in found:
             self.assertEqual(product.name, name)
+
+      def test_find_by_availability(self):
+        """It should Find Products by Availability"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        available = products[0].available
+        count = len([product for product in products if product.available == available])
+        found = Product.find_by_availability(available)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.available, available)
